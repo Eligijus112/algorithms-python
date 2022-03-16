882f2c27f406466ba06e66088d9a247cba6097ec
eligijus <elasorama@gmail.com>
2022-03-16 09:20:32 +0200

    Added the merge sort

--- algorithms/MergeSort/MergeSort.py
+++ algorithms/MergeSort/MergeSort.py
@@ -13,6 +13,14 @@ class MergeSortNode:
         # Saving the length of the array 
         self.n = len(arr)
 
+        # Spliting the nodes further 
+        self.split_node()
+
+        # When creating the node, the initial arr in state is not sorted
+        self.sorted = False
+        if self.n == 1:
+            self.sorted = True
+
     def split_node(self):
         """
         Splits the array into two parts
@@ -29,16 +37,18 @@ class MergeSortNode:
             # right nodes
             self.left = MergeSortNode(left_arr)
             self.right = MergeSortNode(right_arr)
-        else: 
-            # This indicates that the current node is the bottom one
-            self.left = None 
-            self.right = None 
 
-    def merge(self):
+    def merge_sort(self):
         """
         Merges the two nodes to the left and on the right
         """
-        if self.n > 1:
+        if not self.left.sorted: 
+            self.left.merge_sort()
+        
+        if not self.right.sorted:
+            self.right.merge_sort()
+
+        if self.left.sorted and self.right.sorted:
             # Extracting the arrays
             _left_arr = self.left.arr
             _right_arr = self.right.arr
@@ -47,21 +57,43 @@ class MergeSortNode:
             _n = len(_left_arr)
             _k = len(_right_arr)
 
+            # final array length
+            _N = _n + _k
+
             # Creating the empty sorted array 
-            _arr_sorted = [None] * (_n + _k)
+            _arr_sorted = [None] * _N
 
             # The bellow algorithms assumes that both _left_arr and _right_arr
             # are already sorted
-            i = 0
-            j = 0
-            while (i < _n) or (j < _k): 
-                if _left_arr[i] <= _right_arr[j]:
-                    _arr_sorted[i + j] = _left_arr[i]
-                    
-                    i +=1
-                else: 
-                    _arr_sorted[i + j] = _right_arr[j]
+            i = 0 # Iterator for the left array 
+            j = 0 # Iterator for the right array 
+            h = 0 # Iterator for the final array
 
-                    j += 1
+            while h < _N:
+                if j >= _k: 
+                    # This clause means that every element from the right array has been 
+                    # exhausted
+                    for elem in range(i, _n):
+                        _arr_sorted[j + elem] = _left_arr[elem]
+                
+                elif i >= _n: 
+                    # This clause means that every element from the left array has been
+                    # exhaysted 
+                    for elem in range(j, _k):
+                        _arr_sorted[i + elem] = _right_arr[elem]
+                
+                else: 
+                    # This clause means that there are elements in both of the left and right 
+                    # arrays
+                    if _left_arr[i] <= _right_arr[j]:
+                        _arr_sorted[i + j] = _left_arr[i]
+                        i += 1
+                    else: 
+                        _arr_sorted[i + j] = _right_arr[j]
+                        j += 1
+                
+                # Incrementing the h 
+                h += 1
         
-        self.arr = _arr_sorted
+            self.arr = _arr_sorted
+            self.sorted = True
---
+++ tests/test_mergesort.py
@@ -0,0 +1,55 @@
+# Importing the main framework
+from algorithms.MergeSort.MergeSort import MergeSortNode
+
+def test_merge_sort_min_node():
+    """
+    The minimal array of two elements
+    """
+    # Arrange 
+    a = [5, -1]
+
+    # Act 
+    node = MergeSortNode(a)
+    node.merge_sort()
+
+    # Assert 
+    assert node.left.arr == [5]
+    assert node.right.arr == [-1]
+    assert node.arr == [-1, 5]
+
+def test_node_spliting():
+    """
+    Test if the node spliting is correct
+    """
+    # Arrange 
+    a = [5, -1, -99, 168, 54, 12, 3]
+
+    # Act 
+    node = MergeSortNode(a)
+
+    # Assert 
+    assert node.left.arr == [5, -1, -99]
+    assert node.right.arr == [168, 54, 12, 3]
+
+    assert node.left.left.arr == [5]
+    assert node.left.left.sorted 
+    
+    assert node.left.right.arr == [-1, -99]
+    assert not node.left.right.sorted
+
+    assert node.right.left.arr == [168, 54]
+    assert node.right.right.arr == [12, 3]
+
+def test_merge_sort_full():
+    """
+    Tests the full implementation of merge sort 
+    """
+    # Arrange 
+    a = [5, -1, -99, 168, 54, 12, 3]
+
+    # Act 
+    node = MergeSortNode(a)
+    node.merge_sort()
+
+    # Asserting 
+    assert node.arr == [-99, -1, 3, 5, 12, 54, 168]
