51a2817186133f5872a7ca9cae417bcfbc490a46
eligijus <elasorama@gmail.com>
2022-03-16 18:24:01 +0200

    Added the initial quick sort implementation

---
+++ algorithms/QuickSort/QuickSort.py
@@ -0,0 +1,64 @@
+class QuickSort:
+    def __init__(self, arr):
+        # Ensuring the correct type 
+        if type(arr) != list:
+            raise TypeError("Expected a list")
+        
+        # Saving the array to memory 
+        self.arr = arr
+
+        # Saving the length of the array 
+        self.n = len(arr)
+
+        # Indication whether the current element is partitioned or not 
+        self.sorted = False
+        if self.n <= 1:
+            self.sorted = True 
+
+        # Partitioning 
+        self.partition()
+
+    def partition(self):
+        """
+        In this implementation we will use the right most element as a pivot
+        """
+        if self.n > 1:
+            # Extracting the right most element 
+            _pivot = self.arr[-1]
+
+            # Saving the pivot element to memory 
+            self.pivot = [_pivot] 
+
+            # Creating the left and right lists
+            _left_arr = []
+            _right_arr = []
+
+            for i in range(self.n - 1):
+                # The item to check 
+                _ith_element = self.arr[i]
+
+                # Populating the right and left elements
+                if _ith_element <= _pivot:
+                    _left_arr.append(_ith_element)
+                else: 
+                    _right_arr.append(_ith_element)
+            
+            # Recursevly partitioning the gotten lists
+            self.left = QuickSort(_left_arr)
+            self.right = QuickSort(_right_arr)
+    
+    def sort(self):
+        """
+        Merges the left and right arrays. 
+        The element between the two lists is the _pivot element
+        """
+        if not self.left.sorted:
+            self.left.sort()
+
+        if not self.right.sorted:
+            self.right.sort()
+
+        if self.left.sorted and self.right.sorted:
+            self.arr = self.left.arr + self.pivot + self.right.arr 
+
+        self.sorted = True
